71c7ea0ae73182e63690a5acbe66e291388dc6f9
Charlie <charln2@uw.edu>
2017-05-07 15:49:10 -0700

    Raise minsdkversion and add AuthUI dependencies

--- app/src/main/java/com/charln2/morsible/MainActivity.java
+++ app/src/main/java/com/charln2/morsible/MainActivity.java
@@ -1,26 +1,70 @@
 package com.charln2.morsible;
 
 import android.media.MediaPlayer;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.Touch;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import static android.R.attr.button;
 import static android.R.attr.id;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity Class";
+    //UI/ Resources
     private MediaPlayer mp;
     private Button b;
+
+    //Firebase Components
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener == null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mp = MediaPlayer.create(this, R.raw.tone_600hz);
+        // Init Firebase Components
+        mAuth = FirebaseAuth.getInstance();
 
+        // Init Listeners
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        // Init UI/Resources
+        mp = MediaPlayer.create(this, R.raw.tone_600hz);
         b = (Button) findViewById(R.id.button);
         b.setOnTouchListener(new View.OnTouchListener() {
             @Override
