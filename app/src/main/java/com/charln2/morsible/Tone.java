8491b5a8f6fa61f45078040b21a793ec2414c0b0
Charlie <charln2@uw.edu>
2017-05-14 10:59:25 -0700

    Add Tone.java class

---
+++ app/src/main/java/com/charln2/morsible/Tone.java
@@ -0,0 +1,51 @@
+package com.charln2.morsible;
+
+import android.graphics.Color;
+
+import static android.graphics.Color.CYAN;
+
+/**
+ * Created by charl on 5/11/2017.
+ */
+
+public class Tone {
+    private boolean isButtonActivated;
+    private int soundId;
+    private int highlightColor;
+
+    public Tone() {
+        isButtonActivated = false;
+        soundId = R.raw.tone_600hz;
+        highlightColor = Color.CYAN;
+    }
+
+    public Tone(boolean buttonActive, int soundId, int highlightColor) {
+        this.isButtonActivated = buttonActive;
+        this.soundId = soundId;
+        this.highlightColor = highlightColor;
+    }
+
+    public void setButtonActivated(boolean buttonActivated) {
+        isButtonActivated = buttonActivated;
+    }
+
+    public void setSoundId(int soundId) {
+        this.soundId = soundId;
+    }
+
+    public void setHighlightColor(int highlightColor) {
+        this.highlightColor = highlightColor;
+    }
+
+    public boolean isActive() {
+        return isButtonActivated;
+    }
+
+    public int getSoundId() {
+        return soundId;
+    }
+
+    public int getHighlightColor() {
+        return highlightColor;
+    }
+}
