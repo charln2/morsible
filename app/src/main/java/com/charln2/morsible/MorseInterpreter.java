34335d7567e34398ea31ff8c75b0765820aca719
Charlie <charln2@uw.edu>
2017-10-11 11:37:00 -0700

    Change style and set up time constants for taps

--- app/src/main/java/com/charln2/morsible/MorseInterpreter.java
+++ app/src/main/java/com/charln2/morsible/MorseInterpreter.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -15,21 +16,28 @@ import static com.facebook.FacebookSdk.getApplicationContext;
  */
 
 public class MorseInterpreter {
+    private final String LOG_TAG = getClass().getSimpleName();
     private static final int BORDER_WIDTH = 16;
+    private final int DOT_DURATION = 50; // milliseconds
+    private final int DASH_DURATION = DOT_DURATION * 3; // milliseconds
+    private final int LETTER_DURATION = DOT_DURATION * 3; // milliseconds
+    private final int SPACE_DURATION = DOT_DURATION * 7; // milliseconds
+
+    private long lastStartTime;
+    private long lastEndTime;
+    private StringBuilder workingChar; // stores dots and dashes to be parsed into chars
+
 
     public Activity activityMain; // to grab UI and Firebase elements
+
     private User mUser;
     // Todo: button logic
     private Button b;
-
-    // Todo: countdown timer logic
-    private StringBuffer sb; // array to store touch input
 
     public MorseInterpreter(Activity _activity, User user) { // User, fbRef
         this.activityMain = _activity;
         this.mUser = user;
 
-        sb = new StringBuffer();
         b = (Button) activityMain.findViewById(R.id.button);
         final GradientDrawable gd = (GradientDrawable) b.getBackground();
         b.setOnTouchListener(new View.OnTouchListener() {
@@ -43,6 +51,8 @@ public class MorseInterpreter {
                         gd.setStroke(BORDER_WIDTH, Color.parseColor(mUser.getHighlightColor())); //change
 
                         // todo: parse button input via timer, append to sb, update FB val
+                        lastStartTime = System.currentTimeMillis();
+                        Log.v(LOG_TAG, "lastStartTime currentTimeMillis: " + lastStartTime);
                         break;
                     case MotionEvent.ACTION_UP:
                         mUser.setButtonActivated(false); // disable tone
@@ -50,13 +60,29 @@ public class MorseInterpreter {
                         gd.setStroke(BORDER_WIDTH,
                                 ContextCompat.getColor(getApplicationContext(),
                                         R.color.tw__transparent)); // reset border indicateorcolor
+
+//                        if (lastEndTime != null) {
+//
+//                        }
+                        long timeIdle = parseTimeIdle(); // check letter, check space,
+                        if(timeIdle <= LETTER_DURATION) {
+
+                        }
+                        if(timeIdle <= SPACE_DURATION) {
+
+                        }
+                        lastEndTime = System.currentTimeMillis();
                         break;
                 }
+
                 return false;
             }
         });
     }
 
+    private long parseTimeIdle() {
+        return (lastEndTime != 0) ? lastEndTime - lastStartTime : 0;
+    }
 
     public void append(char ch) {
         //todo: append ".", "_", or "\b"
